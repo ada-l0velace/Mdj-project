89dd1f84bc9158031882f6e804de9186d374a783
lolstorm92 <ricardo.s.campos@tecnico.ulisboa.pt>
2019-04-12 04:52:52 +0100

    Some progress on #6 and #14 towers can be built and placed and there's a list of grid objects right now

--- Assets/scripts/script.cs
+++ Assets/scripts/script.cs
@@ -5,29 +5,66 @@ using UnityEngine.UI;
 
 public class script : MonoBehaviour
 {
+    public bool buildTower;
     public Button button;
     public GameObject tower;
+    private GameObject currTower;
     public GameObject enemy;
     private int count = 0;
+    private Vector3 currentPos;
     private GameObject prev;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        currentPos = new Vector3(0,0,0);
         button.onClick.AddListener(this.clicked);
     }
 
     void clicked()
     {
+        buildTower = true;
+        
         GameObject e = (GameObject) Instantiate(tower);
-        e.transform.SetParent(gameObject.transform);
-        e.transform.localPosition = new Vector3(Random.Range(-4, 4), 1, Random.Range(-4, 4));
-    
+        currTower = e;
+        //e.transform.SetParent(gameObject.transform);
+        //e.transform.localPosition = new Vector3(Input.mousePosition.x, 1, Input.mousePosition.z);
+        /*
         count++;
-        Debug.Log("Count: " + count);
+        Debug.Log("Count: " + count);*/
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity) && currTower) {
+                currTower.transform.position = hit.point;
+                //Debug.DrawRay(hit);
+                //Debug.Log(hit.transform.position);
+                currTower = null;
+            }
+        }
+        else if (currTower != null) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity) && currTower) {
+                currentPos.x = hit.point.x;
+                currentPos.z = hit.point.z;
+                currTower.transform.position = currentPos;
+                //Debug.DrawRay(hit);
+                //Debug.Log(hit.transform.position);
+            }
+        }
+        //Debug.Log(Input.mousePosition);
+    }
+
+    void OnMouseOver() {
+        /*Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("WTFF");
+        if (buildTower) {
+            currTower.transform.localPosition = new Vector3(pz.x, 1, pz.z);
+        }*/
     }
 }
