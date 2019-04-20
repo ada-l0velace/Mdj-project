74e0f6a271037260de3df74875e514588870b39f
Francisco do Carmo <franciscodocarmo16@gmail.com>
2019-04-20 18:58:00 +0100

    Main Menu

---
+++ Assets/scripts/SceneChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Change a scene.
+/// </summary>
+public class SceneChanger : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the scene.
+    /// </summary>
+    /// <param name="sceneName">Scene name.</param>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Loads the current scene.
+    /// </summary>
+    public void LoadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitApp()
+    {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit ();
+    #endif
+    }
+}
